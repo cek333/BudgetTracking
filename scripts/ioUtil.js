168a4aa3b18b9310a58437ecea0a5b9cb2f64754
Cecil <69444825+cek333@users.noreply.github.com>
2021-09-10 22:21:23 -0400

    helper functions to convert each line of csv data to array (csvToArray) + script to convert csv array data to json (csvArrToJson)

--- scripts/ioUtil.js
+++ scripts/ioUtil.js
@@ -20,6 +20,46 @@ function dataToRows(dataIn, removeHdr) {
   return dataAsRows;
 }
 
+function csvToArray(dataIn) {
+  return dataIn.map(csvRow => {
+    // Replace any quoted text. Only expecting one quoted string per line.
+    const quoteCheck = csvRow.match(/"(.*?)"/);
+    // console.log(quoteCheck);
+    let lineNoText;
+    let text = null;
+    if (quoteCheck) {
+      text = quoteCheck[1];
+      // Replace quoted string with placeholder string
+      lineNoText = csvRow.replace(text, '###text###');
+    } else {
+      lineNoText = csvRow;
+    }
+    // Split by commas
+    let csvArr = lineNoText.split(',');
+    // Re-insert quoted text
+    if (text !== null) {
+      csvArr = csvArr.map(col => col.replace('###text###', text));
+    }
+    return csvArr;
+  });
+}
+
+function csvArrToJson(arrOfRows, keys) {
+  // arrOfRows = [ [a,b,c], [a,b,c] ]
+  // keys: [ key1, key2, key3 ]
+  // Check for mismatch in array lengths
+  if (keys.length !== arrOfRows[0].length) {
+    return [];
+  }
+  return arrOfRows.map(csvArr => {
+    const asJson = { };
+    for (let idx = 0; idx < keys.length; idx++) {
+      asJson[keys[idx]] = csvArr[idx];
+    }
+    return asJson;
+  });
+}
+
 function writeDataToFile(fileOut, data) {
   try {
     fs.writeFileSync(fileOut, data);
@@ -28,4 +68,4 @@ function writeDataToFile(fileOut, data) {
   }
 }
 
-module.exports = { getDataFromFile, dataToRows, writeDataToFile };
+module.exports = { getDataFromFile, dataToRows, writeDataToFile, csvToArray, csvArrToJson };
